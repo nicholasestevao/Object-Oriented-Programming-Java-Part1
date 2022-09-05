d853707a100a48aedebde89ae46b1b9b8da4e5cc
Isaac Soares <isaacsantos.soares@hotmail.com>
2022-09-04 23:48:49 -0300

    Format Update

--- Lista 1/ex3-Lista1/Termo.cpp
+++ Lista 1/ex3-Lista1/Termo.cpp
@@ -13,7 +13,7 @@
 
 #include "Termo.h"
 
-Termo::Termo (int grau, int cte) {
+Termo::Termo(int grau, int cte) {
     this->grau = grau;
     this->cte = cte;
 }
@@ -26,7 +26,7 @@ int Termo::getGrau() {
     return grau;
 }
 
-void Termo::addCte (int add_cte) {
+void Termo::addCte(int add_cte) {
     cte += add_cte;
 }
 
