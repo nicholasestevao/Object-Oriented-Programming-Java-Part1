1819d36c08da46eb9294a271a64eab7c02af657f
nicholasestevao <nicholas.estevao@usp.br>
2022-09-05 09:27:41 -0300

    ex 1 lista 2

---
+++ Lista 2/ex1/Model/Cliente.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Cliente {
+
+    /**
+     * Default constructor
+     */
+    public Cliente() {
+    }
+
+    /**
+     * 
+     */
+    private int cpf;
+
+}
